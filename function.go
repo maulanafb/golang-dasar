5c1478dd7f96ce2f78aa09d7073ad6aa6bc3f54d
unknown <gagas112212@gmail.com>
2024-01-13 10:12:00 +0700

    variadic function

--- function.go
+++ function.go
@@ -8,6 +8,16 @@ func main() {
 	getHello("Maulana")
 	getHello := getHello("Maulana")
 	fmt.Println(getHello)
+	firstName, lastName := getFullName()
+	fmt.Println(firstName, lastName)
+
+	// multiple value harus diambil semua valuenya
+	// jika ingin mengambil 1 value saja , bisa mengganti yang tidak di inginkan dengan(_) garis bawah
+	shortName, _ := getFullName()
+	fmt.Println(shortName)
+
+	a, b, c := getCompleteName()
+	fmt.Println(a, b, c)
 }
 
 func sayHello(firstName string, lastName string) string {
@@ -17,4 +27,16 @@ func sayHello(firstName string, lastName string) string {
 
 func getHello(name string) string {
 	return "Hello " + name
+}
+
+// function return multiple values
+func getFullName() (string, string) {
+	return "maulana", "fatih"
+}
+
+func getCompleteName() (firstName string, middleName string, lastName string) {
+	firstName = "maulana"
+	middleName = "fatih"
+	lastName = "bilqisthi"
+	return firstName, middleName, lastName
 }
---
+++ variadic-function.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sumAll(numbers ...int) int {
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(sumAll(10, 10, 10))
+}
